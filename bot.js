7405688a18437b8ef52d3bb3b39d894c99923f9d
Yanu403 <66066867+Yanu403@users.noreply.github.com>
2024-09-06 10:31:30 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -241,12 +241,30 @@ async function claimFriend() {
       return;
     }
 
-    if (res?.point) {
-      logs(`Klaim berhasil, saldo: ${colors.yellow(new Intl.NumberFormat('us-US').format(res?.point))}`);
+    if (res) {
+      logs('Berhasil klaim poin dari teman.');
+      return;
     } else {
-     
-      logs('Tidak ada poin yang dapat diklaim.'); } } catch (error) { errors('Gagal klaim poin dari teman: ' + error.message); } }
+      logs('Tidak ada poin yang dapat diklaim.');
+    }
+  } catch (error) {
+    errors('Gagal klaim poin dari teman: ' + error.message);
+  }
+}
 
-// Main Function async function main() { await login(); await doQuest(); const friendClaims = await checkFriendClaim(); if (friendClaims) await claimFriend(); }
+// Main Function
+async function main() {
+  await login();
+  await doQuest();
+  const friendClaims = await checkFriendClaim();
+  if (friendClaims) await claimFriend();
+}
 
-(async () => { await setCurrentProject('sunkong'); await setCurrentProfile({ query_id: 'user=%7B%22id%22%3A7194869909%2C%22first_name%22%3A%22X%22%2C%22last_name%22%3A%22X%22%2C%22username%22%3A%22freakz666%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-4906777519212880996&chat_type=supergroup&start_param=FE4E2F48B&auth_date=1725532349&hash=0d56ba18a795ecf7dbb0d2633b7c6e06b11910afa77e4667e10a59bd5ee09a7c', // Silakan ganti dengan query_id Anda token: 'your_token_here', // Silakan ganti dengan token Anda }); await main(); })();
+(async () => {
+  await setCurrentProject('sunkong');
+  await setCurrentProfile({
+    query_id: 'user=%7B%22id%22%3A7194869909%2C%22first_name%22%3A%22X%22%2C%22last_name%22%3A%22X%22%2C%22username%22%3A%22freakz666%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-4906777519212880996&chat_type=supergroup&start_param=FE4E2F48B&auth_date=1725532349&hash=0d56ba18a795ecf7dbb0d2633b7c6e06b11910afa77e4667e10a59bd5ee09a7c', // Silakan ganti dengan query_id Anda
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo3MTk0ODY5OTA5fQ.79zqUaKjOetviPf83XMqbsUB96z5bdGsMGJfucTWGtk', // Silakan ganti dengan token Anda
+  });
+  await main();
+})();
